c80fc1c4bb17ab56769aeafb1531b1995cb5dcfd
SirYorch <jcuevam3@est.ups.edu.ec>
2025-07-25 09:44:33 -0500

    Actualizacion final, proximo son cambios pequeños, readme, y referencias a git de demás usuarios

--- relabeled.py
+++ relabeled.py
@@ -1,5 +1,5 @@
 import os
-##Codigo para reescribir todas las etiquetas a 0. "CAR"
+
 labels_path1 = "vehicles.v2-release.yolov8-obb/train/labels/"
 labels_path2 = "vehicles.v2-release.yolov8-obb/test/labels/"
 labels_path3 = "vehicles.v2-release.yolov8-obb/valid/labels/"
@@ -14,10 +14,11 @@ def relabel(labels_path):
                 new_lines = []
                 for line in lines:
                     parts = line.strip().split()
-                    if parts and parts[0] == '4':
-                        parts[0] = '0' 
-                        new_lines.append(' '.join(parts) + '\n')
+                    parts[0] = '0' 
+                    new_lines.append(' '.join(parts) + '\n')
                 with open(file_path, 'w') as f:
                     f.writelines(new_lines)
+    print(f"Relabeling completed for {labels_path}")
 
 relabel(labels_path3)
+
